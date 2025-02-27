19970d1d814c281b70855440935315c090a86bd1
Iván Della Ventura López (ext) <ivan.ventura.external@eviden.com>
2025-02-27 08:28:14 +0000

    Revert "Merge branch 'development' of https://github.com/novadevseu/blog-nova-devs into development"
    This reverts commit 0686f739f829a54fe03c56edf842cd37b520f096, reversing
    changes made to 20d0db606138415399d34bd8fb506ca19635af8a.

--- src/hooks/editUserHook.ts
+++ src/hooks/editUserHook.ts
@@ -1,43 +1,52 @@
 import { db } from "@/config/firebase-config";
 import { setUser, UserType } from "@/redux/slices/userSlice";
-import { doc, getDoc, updateDoc } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "@firebase/firestore";
 import { Dispatch } from "@reduxjs/toolkit";
-import { useUser } from "./useUser";
 
-/**
- * Actualiza el documento del usuario en Firestore con los datos proporcionados.
- * Finalmente, despacha una acción para actualizar el estado del usuario en Redux,
- * muestra una confirmación y recarga la página.
- *
- * @param dispatch - Función dispatch de Redux.
- * @param userId  - 
- * @param updateUserData - Objeto parcial de UserType con los valores actualizados (ej. email, linkedIn, etc.)
- */
 export const editUserData = async (
-  dispatch: Dispatch, userId : string,
+  dispatch: Dispatch,
   updateUserData: Partial<UserType>
 ) => {
-  // Recupera el ID del usuario desde localStorage.
-  
+  const userId = localStorage.getItem("uid");
 
   if (!userId) {
     return null;
   }
 
   try {
-    // Obtiene una referencia al documento del usuario en Firestore.
     const userDocRef = doc(db, "users", userId);
     const userDoc = await getDoc(userDocRef);
 
     if (userDoc.exists()) {
-      // Actualiza el documento del usuario con los nuevos datos.
       await updateDoc(userDocRef, updateUserData);
 
-      // Vuelve a obtener los datos actualizados del usuario.
-      const updatedUserDoc = await getDoc(userDocRef);
-      const newUserData = updatedUserDoc.data() as UserType;
+      const newUserData = userDoc.data() as UserType;
 
-      // Despacha la acción para actualizar el estado del usuario en Redux.
+      const emailData = updateUserData.email;
+
+      if (emailData) {
+        const commentsQuery = query(
+          collection(db, "comments"),
+          where("email", "==", emailData)
+        );
+        const querySnapshot = await getDocs(commentsQuery);
+
+        const updatePromises = querySnapshot.docs.map(async (commentDoc) => {
+          const commentRef = doc(db, "comments", commentDoc.id);
+          await updateDoc(commentRef, { email: emailData });
+        });
+
+        await Promise.all(updatePromises);
+      }
+
       dispatch(
         setUser({
           role: newUserData.role,
@@ -47,26 +56,15 @@ export const editUserData = async (
           fullName: newUserData.fullName || "",
           img: newUserData.img || "",
           profile: newUserData.profile || "",
-          linkedIn: newUserData.linkedIn || "",
-          bio: newUserData.bio || "",
-          company: newUserData.company || "",
-          education: newUserData.education || "",
-          jobDescription: newUserData.jobDescription || "",
-          skills: newUserData.skills || "",
-          subscribed: newUserData.subscribed || false,
         })
       );
-
-      // Muestra la confirmación de que se ha guardado exitosamente.
-      alert("Your changes have been saved successfully.");
-
-      // Recarga la página para reflejar las actualizaciones.
-      window.location.reload();
     } else {
       return null;
     }
   } catch (error) {
     console.error("Error loading user data:", error);
     return null;
+  } finally {
+    window.location.reload();
   }
 };
